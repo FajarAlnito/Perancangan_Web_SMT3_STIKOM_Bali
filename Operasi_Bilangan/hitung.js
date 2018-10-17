68f350df81832990e641bdd73194359cdf3c0ee0
FajarAlnito <fajaralnito18@gmail.com>
2018-10-17 15:36:11 +0800

    Add First Task

---
+++ Operasi_Bilangan/hitung.js
@@ -0,0 +1,13 @@
+
+            function hitung()
+                {
+                    var a=parseInt(document.getElementById("bil1").value);
+                    var b=parseInt(document.getElementById("bil2").value);
+                    var sum= a+b;
+                    var mpy= a*b;
+                    var sub= a-b;
+                    var dvd= a/b;
+                
+                        alert(a+" + "+b+" = "+sum+"\n"+a+" - "+b+" = "+sub+"\n"+a+" * "+b+" = "+mpy+"\n"+a+" / "+b+" = "+dvd);
+
+                }
